95534dc5322d47612c77fb8792f25eabb1dc3f6f
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-20 22:57:22 +0300

    main.h

--- main.h
+++ main.h
@@ -4,9 +4,21 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
+/**
+ * struct printer - pairs a format specifier with a function
+ * @spec: the specifier character
+ * @func: pointer to the function that handles it
+ */
+typedef struct printer
+{
+    char spec;
+    int (*func)(va_list ap);
+} printer_t;
+
 int _putchar(char c);
-int print_int(va_list args);
-int print_unsigned(unsigned int n);
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_int(va_list ap);
 
 #endif /* MAIN_H */
