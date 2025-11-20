017f7c4a933023d6d892f33096bf788f5a10af59
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-21 01:57:02 +0300

    print_unsigned.c

--- print_unsigned.c
+++ print_unsigned.c
@@ -8,19 +8,19 @@
  */
 int print_unsigned(unsigned long int n)
 {
-    int count = 0;
-    int res;
+	int count = 0;
+	int res;
 
-    if (n / 10)
-    {
-        res = print_unsigned(n / 10);
-        if (res == -1)
-            return (-1);
-        count += res;
-    }
+	if (n / 10)
+	{
+		res = print_unsigned(n / 10);
+		if (res == -1)
+			return (-1);
+		count += res;
+	}
 
-    if (_putchar((n % 10) + '0') == -1)
-        return (-1);
+	if (_putchar((n % 10) + '0') == -1)
+		return (-1);
 
-    return (count + 1);
+	return (count + 1);
 }
